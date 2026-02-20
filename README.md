bd886e554444f3cc9ef6b28d2924e781dca4d19c
Sylphux <masip.pablo@gmail.com>
2026-02-20 02:31:28 +0100

    Corrected errors in PR and reorganised code

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a simple shell install script for Stoat, an open source alternative for discord.
 
-At the moment there is no support for package managers so this script will install stoat from the specified release on github.
+At the moment, Stoat is not supported by most package managers so this script will install it from the latest release on github.
 
 # Usage
 
@@ -18,19 +18,30 @@ If you want the simplest experience possible, you can copy and paste this comman
 git clone https://github.com/Sylphux/stoat_install_script && cd stoat_install_script && chmod +x stoat_install.sh && ./stoat_install.sh
 ```
 
-This will clone the repo and install stoat.
+This will clone the repo and install Stoat.
 
 ## How to start the script
+
+Clone the repo and use the following commands.
 
 ```shell
 chmod +x stoat_install.sh
 ./stoat_install.sh
 ```
 
+## Settings 
+
+Some variables can be changed in the script to suit your needs.
+
+# Contributions
+
+If you want to contribute to this project, please do it on a new branch and then send the PR.
+All contributions are greatly appreciated !
+
 # About 
 
 This is not affiliated with the Stoat team.
-All files downloaded comes from the official Stoat repos.
+All downloaded files comes from the official Stoat repos.
 
 Feel free to contact me for bug reports or advices.
 You can also give feedback on the reddit post : https://www.reddit.com/r/stoatchat/comments/1r98j9a/comment/o6bl1wm/
--- stoat_install.sh
+++ stoat_install.sh
@@ -1,36 +1,47 @@
-# All these variables can be edited to customize your installation.
+VERSION=1.3.0 # Fallback version if auto latest release detection is not used. This can be changed if necessary.
+
+echo "Welcome to this basic Stoat install/update script for Linux."
+echo "Would you like to find the latest release available ? If not, the script will default to version $VERSION."
+read -p "Search for latest release ? (Y/n): " ask
+echo ""
+if [ "$ask" != "n" ]; then
+    echo "Please wait while script finds the latest release version..."
+    FOUND_VERSION=$(searchitem=$(curl -v https://github.com/stoatchat/for-desktop 2>&1 | grep -E '\<span\sclass\=\"css-truncate\scss\-truncate-target\stext-bold\smr-2"\sstyle=\"max-width\:\snone\;\">v([0-9]\.[0-9]+\.[0-9]+)<\/span\>'); grep -Po '([0-9]\.[0-9]+\.[0-9]+)' <<< $searchitem)
+    if ! [[ "$FOUND_VERSION" =~ [0-9]\.[0-9]+\.[0-9]+ ]]; then
+        echo "Error retrieving latest version. Defaulting to $VERSION"
+    else
+        VERSION="$FOUND_VERSION"
+        echo ""
+        echo "Version $VERSION was automatically detected as the latest release. It will be downloaded and installed/updated."
+    fi
+else
+    echo "Defaulting to version $VERSION. This can be changed on the first line of the script."
+fi
+
+# All variables below this line can be edited to customize your installation.
 
 INSTALL_PATH="$HOME/.local/bin/"
 FILE="Stoat-linux-x64-$VERSION.zip"
+ICON_PATH="$INSTALL_PATH/Stoat-linux-x64/icon.ico"
+
+# Don't touch anything below here unless you know what you are doing.
+
 RELEASE_URL="https://github.com/stoatchat/for-desktop/releases/download/v$VERSION/Stoat-linux-x64-$VERSION.zip"
 ICON_URL="https://raw.githubusercontent.com/stoatchat/assets/refs/heads/main/desktop/icon.ico"
-ICON_PATH="$INSTALL_PATH/Stoat-linux-x64/icon.ico"
 DESKTOP_FILE="$HOME/.local/share/applications/stoat.desktop"
 
-# Don't touch anything below here unelss you know what you are doing
-
-echo "Hello, and welcome to this basic Stoat install/update script for Linux."
-echo "Please wait while script finds the latest release version..."
-VERSION=$(searchitem=$(curl -v https://github.com/stoatchat/for-desktop 2>&1 | grep -E '\<span\sclass\=\"css-truncate\scss\-truncate-target\stext-bold\smr-2"\sstyle=\"max-width\:\snone\;\">v([0-9]\.[0-9]+\.[0-9]+)<\/span\>'); grep -Po '([0-9]\.[0-9]+\.[0-9]+)' <<< $searchitem)
-if ! [[ "$VERSION" =~ [0-9]\.[0-9]+\.[0-9]+ ]]; then
-    echo "Version get failed."
-    echo "Exiting..."
-    exit
-fi
-echo "Version $VERSION was automatically detected as the latest release. It will be downloaded and installed/updated."
 echo "Be sure to have chromium and all the required dependencies installed before proceeding."
 echo ""
-read -sp "Continue ? (Y/n): " ask
+read -p "Continue ? (Y/n): " ask
 if [ "$ask" = "n" ]; then
     echo "Exiting."
     exit
 fi
-output=""
 wget -O $FILE $RELEASE_URL && echo "Downloaded $FILE" && output="success" || output="failed"
 if [ "$output" != "success" ]; then
     echo "Download failed. Check for release version or create an issue in the repo."
     rm -rf $FILE
-    echo "Exiting..."
+    echo "Exiting."
     exit
 fi
 echo "Downloaded $FILE successfully."
